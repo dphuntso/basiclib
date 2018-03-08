e6a955a05a0823023d01a53550732f8dd33fd415
dorjee666 <33818256+dorjee666@users.noreply.github.com>
2018-03-08 01:53:13 -0800

    Add files via upload

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: dphuntso <dphuntso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/28 17:54:18 by dphuntso          #+#    #+#             */
-/*   Updated: 2018/03/07 18:02:36 by dphuntso         ###   ########.fr       */
+/*   Updated: 2018/03/08 01:48:52 by dphuntso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	ft_lstiter(t_list *lst, void (*f)(t_list *elem))
 {
+	if (lst == NULL || f == NULL)
+		return ;
 	if (lst)
 		f(lst);
 	while (lst->next)
